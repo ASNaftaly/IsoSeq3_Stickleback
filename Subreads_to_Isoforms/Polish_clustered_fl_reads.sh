3aad8a264c5039a7412bae98ec17c7cf9dc5d69f
Alice <alice.shanfelter@gmail.com>
2020-04-08 10:10:25 -0400

    added script for polishing clustered reads

---
+++ Subreads_to_Isoforms/Polish_clustered_fl_reads.sh
@@ -0,0 +1,7 @@
+#program: Isoseq3 v3.1 via conda & SMRTlink v6
+
+#command:
+#need to remove <> prior to running
+
+isoseq3 polish -j < # of threads to use> --verbose <output.unpolished.clustered.bam> <raw.subreads.bam> <output.polished.clustered.bam>
+
