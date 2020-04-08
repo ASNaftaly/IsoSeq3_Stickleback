c75e9f7a38f3024805cf05e9c41002bd77560246
Alice <alice.shanfelter@gmail.com>
2020-04-08 09:23:17 -0400

    added script for creating ccs xml file

---
+++ Subreads_to_Isoforms/Create_CCS_xml.sh
@@ -0,0 +1,7 @@
+#program: smrtlink/6.0.0.47841
+
+#command:
+#need to remove <> prior to running script
+#can add multiple ccs reads.bam files at end of script
+
+dataset create --type ConsensusReadSet <output.consensus.readset.xml> <output.ccs.bam>
