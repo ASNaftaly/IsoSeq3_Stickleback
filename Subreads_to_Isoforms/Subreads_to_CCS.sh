7e639b112c8b97b0d41126fe36b2356c3c53b6fc
Alice <alice.shanfelter@gmail.com>
2020-04-08 08:56:21 -0400

    script to convert subreads to CCS using smrtlink ccs

---
+++ Subreads_to_Isoforms/Subreads_to_CCS.sh
@@ -0,0 +1,8 @@
+#program version: smrtlink/6.0.0.47841
+
+#command:
+#will need to remove <> to run script
+#used isoseq version 3.0
+
+
+ccs <subreads.xml file> --noPolish --minPasses 1 --numthreads 24 <output.ccs.bam>
