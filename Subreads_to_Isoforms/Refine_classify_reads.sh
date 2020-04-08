630677218cbee5494c904c1b08dbf16c1aa15b27
Alice <alice.shanfelter@gmail.com>
2020-04-08 11:19:52 -0400

    added script to run refine to classify full length reads

---
+++ Subreads_to_Isoforms/Refine_classify_reads.sh
@@ -0,0 +1,6 @@
+#program: Isoseq3 v3.1 via conda
+
+#command:
+#need to remove <> prior to running script
+
+isoseq3 refine --require-polya <output.lima.primer_5p--primer3p.bam> <Primers.fasta> <output.unpolished.flnc.bam>
