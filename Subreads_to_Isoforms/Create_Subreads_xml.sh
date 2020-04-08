b2315b890636bcda2f104616780676f57695f334
Alice <alice.shanfelter@gmail.com>
2020-04-08 09:21:17 -0400

    added script for creating subreads.xml file to speed up analyses

---
+++ Subreads_to_Isoforms/Create_Subreads_xml.sh
@@ -0,0 +1,7 @@
+#program: smrtlink/6.0.0.47841
+
+#command
+#need to remove <> before running
+#can add multiple subreads.bam files at the end of the command
+
+dataset create --type SubreadSet <output.subreads.xml> <subreads.bam> 
