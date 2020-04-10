dc8385d63d9e05606c4687f396198726115dd293
Alice <alice.shanfelter@gmail.com>
2020-04-10 08:45:33 -0400

    Added script to make R script to create BUSCO plot

---
+++ BUSCO_Analysis/BUSCO_Plot.sh
@@ -0,0 +1,9 @@
+#Program: BUSCO v3.0.2
+
+#command:
+#need to remove <> prior to running script
+#I moved the summary files from BUSCO to new directories so only the summaries I wanted would be plotted
+#I had to run the R script created from this script separately after changing a few parameters
+
+python generate_plot.py -wd <working directory with summary files>
+
