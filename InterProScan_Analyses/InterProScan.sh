3a3029551a5a24381799a6295acc08907f1c4204
Alice <alice.shanfelter@gmail.com>
2020-04-15 09:44:24 -0400

    Added parameters for interproscan script

--- InterProScan_Analyses/InterProScan.sh
+++ InterProScan_Analyses/InterProScan.sh
@@ -4,4 +4,4 @@
 #need to remove <> prior to running script
 #protein sequences were used as input
 
-sh interproscan.sh -b <output file> -cpu <number of cpus to use> -i <input faa file> -t p -T <temporary directory for files>
+sh interproscan.sh -b <output file> -cpu <number of cpus to use> -i <input faa file> -t p --goterms --pathways -T <temporary directory for files>
