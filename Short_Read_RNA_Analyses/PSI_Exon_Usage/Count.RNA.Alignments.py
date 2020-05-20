1b702b75fbe2eaccbf13208b4418b71451df52ca
Alice <alice.shanfelter@gmail.com>
2020-05-20 07:46:05 -0400

    altered count script as duplicates function was not needed (there are not duplicate alignments)

--- Short_Read_RNA_Analyses/PSI_Exon_Usage/Count.RNA.Alignments.py
+++ Short_Read_RNA_Analyses/PSI_Exon_Usage/Count.RNA.Alignments.py
@@ -55,41 +55,13 @@ def filter_cover_boundary():
     return reads_cover_boundary, reads_do_not_cover_boundary
 
 
-#remove any potential duplicate read mapping
-#returns 3 lists = 1 = final unique reads across the boundary, duplicate reads across the boundary, reads that do not span the boundary
-def remove_read_duplicates():
-    reads_across_boundary, reads_not_across_boundary = filter_cover_boundary()
-    #identify which reads are duplicated
-    all_reads = set()
-    duplicate_reads = []
-    for idx, val in enumerate(reads_across_boundary):
-        if val not in all_reads:
-            all_reads.add(val)
-        else:
-            duplicate_reads.append(val)
-    duplicate_read_alignments = []
-    final_unique_reads = []
-    #get indeces for duplicate values
-    for overlap in duplicate_reads:
-        duplicate_read_alignments += [i for i, x in enumerate(reads_across_boundary) if x == overlap]
-    #remove indeces from reads_across_boundary
-    for index, k in enumerate(reads_across_boundary):
-        if index in duplicate_read_alignments:
-            continue
-        elif index not in duplicate_read_alignments:
-            final_unique_reads.append(k)
-    return final_unique_reads, duplicate_reads, reads_not_across_boundary
-
-
-
-#count reads across boundary, duplicate reads, and reads not across boundary
+#count reads across boundary and reads not across boundary
 
 #counts non duplicate reads that span boundary
 #will only add reads that fit criteria
 #returns dictionary with key == junction identifier and value == number of reads that fit criteria
 def count_final_reads():
-    print("Filtered out duplicate reads")
-    final_reads, duplicates, reads_not_spanning_boundary = remove_read_duplicates()
+    final_reads, reads_not_spanning_boundary = filter_cover_boundary()
     bed_dict = read_bed()
     final_junction_read_count = {}
     for junction in bed_dict:
@@ -105,29 +77,10 @@ def count_final_reads():
     return final_junction_read_count
 
 
-#counts duplicate reads
-#will return values that have 0 duplicates as well as those that have more
-#returns dictionary with key == junction identifier and value == number of duplicate reads
-def count_duplicate_reads():
-    final_reads, duplicates, reads_not_spanning_boundary = remove_read_duplicates()
-    bed_dict = read_bed()
-    duplicate_read_count = {}
-    for junction in bed_dict:
-        single_junction = bed_dict[junction]
-        aligned_junction_read_count = 0
-        for read in single_junction:
-            read_name = read[2]
-            if read_name in duplicates:
-                aligned_junction_read_count += 1
-        duplicate_read_count.update({junction:str(aligned_junction_read_count)})
-    print("Counted duplicate reads")
-    return duplicate_read_count
-
-
 #count reads that do not span junction
 #this can also produce 0 reads for a given junction, if no reads do not span the exon pair boundary
 def count_nonspanning_boundary_reads():
-    final_reads, duplicates, reads_not_spanning_boundary = remove_read_duplicates()
+    final_reads, reads_not_spanning_boundary = filter_cover_boundary()
     bed_dict = read_bed()
     nonspanning_read_count = {}
     for junction in bed_dict:
@@ -157,21 +110,9 @@ def write_nonduplicate_boundaryspanning_reads():
             out.write(final)
 
 
-def write_duplicate_boundaryspanning_reads():
-    reads_to_count = count_duplicate_reads()
-    output = sys.argv[3]
-    with open(output, 'a') as out:
-        header = "Junction.Identifier\tNo.of.Reads\n"
-        out.write(header)
-        for junction in reads_to_count:
-            single_junction = reads_to_count[junction]
-            final = "%s\t%s\n" % (str(junction), str(single_junction))
-            out.write(final)
-
-
 def write_non_boundaryspanning_reads():
     reads_to_count = count_nonspanning_boundary_reads()
-    output = sys.argv[4]
+    output = sys.argv[3]
     with open(output, 'a') as out:
         header = "Junction.Identifier\tNo.of.Reads\n"
         out.write(header)
@@ -184,7 +125,6 @@ def write_non_boundaryspanning_reads():
 #call all functions
 def call():
     PSI_counts = write_nonduplicate_boundaryspanning_reads()
-    duplicates = write_duplicate_boundaryspanning_reads()
     non_boundaryspanning = write_non_boundaryspanning_reads()
 
 call()
